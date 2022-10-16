154ee429abbe81d72444a49309b1921f71c27c61
Mark Tsyrulnyk <elioni17@gmail.com>
2022-10-16 13:15:06 +0300

    added entrypoint

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use solana_program::{
+    account_info::{AccountInfo},
+    entrypoint,
+    entrypoint::ProgramResult,
+    msg,
+    pubkey::Pubkey,
+};
+
+// Declare and export the program's entrypoint
+entrypoint!(process_instruction);
+
+// Program entrypoint's implementation
+pub fn process_instruction(
+    program_id: &Pubkey, // Public key of the account the hello world program was loaded into
+    accounts: &[AccountInfo], // The account to say hello to
+    instruction_data: &[u8], // Ignored, all helloworld instructions are hellos
+) -> ProgramResult {
+    msg!("Hello World Rust program entrypoint");
+
+    Ok(())
 }
